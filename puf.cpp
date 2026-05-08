5d4a2981b6ecac91c3cd0bd51676802a6cc350c7
Marco Oliani <marco.oliani@studenti.univr.it>
2026-05-08 19:08:35 +0200

    puf.cpp now working with mbedTLS libraries

--- puf.cpp
+++ puf.cpp
@@ -1,29 +1,17 @@
 #include "puf.h"
 
-#include <WiFi.h>
-#include <SHA3.h>
-
-/*
 extern "C" {
-  #include "esp_system.h"
-  #include "esp_wifi.h"
-}
-*/
-
-extern "C" {
-  #include "esp_mac.h"  // Per esp_read_mac() e ESP_MAC_WIFI_STA
+  #include "esp_mac.h"
+  #include "mbedtls/sha3.h"
 }
 
 String getPUF() {
     uint8_t mac[6];
     esp_read_mac(mac, ESP_MAC_WIFI_STA);
 
-    SHA3_256 hasher;
-    hasher.reset();
-    hasher.update(mac, 6);
-
     uint8_t hash[32];
-    hasher.finalize(hash, sizeof(hash));
+
+    mbedtls_sha3(MBEDTLS_SHA3_256, mac, 6, hash, sizeof(hash));
 
     String result;
     char buf[3];
